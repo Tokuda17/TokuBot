80f33708096c6282b2d3bc54ed248caba46d2548
Michael Tokuda <michael.tokuda17@gmail.com>
2024-05-30 01:15:33 -0400

    built inHand() which returns whether an opponent is in hand. need to work on myPlayer

--- bovada.js
+++ bovada.js
@@ -3,28 +3,52 @@ var seats = [];
 
 //function that finds the game html files for bovada
 function init() {
-  game = document
-    .getElementById("root")
-    .getElementsByClassName("f8wu152")[0]
-    .getElementsByClassName("f5ukfdt")[0]
-    .getElementsByClassName("f1djomsr")[0]
-    .getElementsByTagName("div")[0]
-    .getElementsByTagName("iframe")[0]
-    .contentDocument.getElementsByTagName("html")[0]
-    .getElementsByTagName("body")[0]
-    .getElementsByTagName("div")[0]
-    .getElementsByTagName("div")[0]
-    .getElementsByClassName("f1l5nl24")[0]
-    .getElementsByClassName("fmyv4dc")[0]
-    .getElementsByClassName("f1qy5s7k")[0]
-    .getElementsByTagName("div")[0]
-    .getElementsByTagName("div")[3]
-    .getElementsByClassName("f1so0fyt")[0];
+  try {
+    game = document
+      .getElementById("root")
+      .getElementsByClassName("f8wu152")[0]
+      .getElementsByClassName("f5ukfdt")[0]
+      .getElementsByClassName("f1djomsr")[0]
+      .getElementsByTagName("div")[0]
+      .getElementsByTagName("iframe")[0]
+      .contentDocument.getElementsByTagName("html")[0]
+      .getElementsByTagName("body")[0]
+      .getElementsByTagName("div")[0]
+      .getElementsByTagName("div")[0]
+      .getElementsByClassName("f1l5nl24")[0]
+      .getElementsByClassName("fmyv4dc")[0]
+      .getElementsByClassName("f1qy5s7k")[0]
+      .getElementsByTagName("div")[0]
+      .getElementsByTagName("div")[3]
+      .getElementsByClassName("f1so0fyt")[0];
+  } catch (error) {
+    console.log("Trying Again");
+  }
   //console.log(game);
 }
 
 //functions that updates the board and potsize
 function getBoard() {}
+
+function isInHand(player) {
+  var card;
+  try {
+    card = player
+      .getElementsByTagName("div")[1]
+      .getElementsByClassName("f1itp13a")[0]
+      .getElementsByClassName("fcmecz0 fppooe4")[0]
+      .getElementsByClassName("f1l8jkug")[0]
+      .getElementsByClassName("f1jve9ln")[0]
+      .getElementsByClassName("landscape f17v6pzz fplgmm0 f1yx1n71")[0]
+      .getElementsByClassName("landscape f17v6pzz f1x351eq f45h")[0]
+      .getElementsByTagName("svg")[0].dataset.qa;
+  } catch (error) {
+    return false;
+  }
+  //  console.log(card);
+  if (card != "card-1") return true;
+  else return false;
+}
 
 //function that initializes the seats and players in each seat.
 function initSeats() {
@@ -46,10 +70,13 @@ function initSeats() {
       var player = new Player(0, 0, false, false);
       console.log(player);
     } else {
-      console.log("Active Player + " + i);
+      var inHand = isInHand(players[i]);
+      console.log("Active Player + " + (i + 1) + " " + inHand);
     }
   }
 }
+
+//returns whether or not a given player is in the hand
 
 function main() {
   init();
@@ -57,7 +84,7 @@ function main() {
 }
 
 // Run the check every 1 seconds (1000 milliseconds)
-setInterval(main, 10000);
+setInterval(main, 1000);
 
 class Player {
   constructor(bet, stack, isActive, inHand) {
