459f1a9b727a269cd0c78c5334f98f88f5cb5066
Michael Tokuda <michael.tokuda17@gmail.com>
2024-06-03 16:37:25 -0400

    finds when player is active.

--- bovada.js
+++ bovada.js
@@ -1,5 +1,9 @@
 var board;
 var seats = [];
+const playersContainers = new Array(9);
+const players = new Array(9);
+var myPlayer;
+var game;
 
 //function that finds the game html files for bovada
 function init() {
@@ -11,69 +15,72 @@ function init() {
       .getElementsByClassName("f1djomsr")[0]
       .getElementsByTagName("div")[0]
       .getElementsByTagName("iframe")[0]
-      .contentDocument.getElementsByTagName("html")[0]
-      .getElementsByTagName("body")[0]
-      .getElementsByTagName("div")[0]
-      .getElementsByTagName("div")[0]
-      .getElementsByClassName("f1l5nl24")[0]
-      .getElementsByClassName("fmyv4dc")[0]
-      .getElementsByClassName("f1qy5s7k")[0]
-      .getElementsByTagName("div")[0]
-      .getElementsByTagName("div")[3]
-      .getElementsByClassName("f1so0fyt")[0];
+      .contentDocument.getElementsByTagName("html")[0];
+    for (var i = 0; i < 9; i++) {
+      const player = game.querySelector(
+        `#root > div > div.frlfvhr > div.f1l5nl24 > div.fmyv4dc > div.f1qy5s7k > div:nth-child(1) > div.fsusjyu.Desktop.landscape.f1u9jrie > div.f1so0fyt > div:nth-child(${
+          i + 2
+        })`
+      );
+      if (player.getElementsByClassName("myPlayer")[0]) myPlayer = i;
+      playersContainers[i] = player;
+    }
   } catch (error) {
     console.log("Trying Again");
   }
-  //console.log(game);
 }
 
 //functions that updates the board and potsize
 function getBoard() {}
 
-function isInHand(player) {
-  var card;
-  try {
-    card = player
-      .getElementsByTagName("div")[1]
-      .getElementsByClassName("f1itp13a")[0]
-      .getElementsByClassName("fcmecz0 fppooe4")[0]
-      .getElementsByClassName("f1l8jkug")[0]
-      .getElementsByClassName("f1jve9ln")[0]
-      .getElementsByClassName("landscape f17v6pzz fplgmm0 f1yx1n71")[0]
-      .getElementsByClassName("landscape f17v6pzz f1x351eq f45h")[0]
-      .getElementsByTagName("svg")[0].dataset.qa;
-  } catch (error) {
-    return false;
+function isInHand() {}
+
+function initPlayer(seat) {
+  var player = new Player(0, 0, "ACTIVE", "INHAND");
+  // var balance = playersContainers[seat].querySelector(
+  //   "div > div:nth-child(2) > div:nth-child(3) > div.f1ay1w8p.leftPlayer.notZone.fimvvv > div > span"
+  // );
+  // var balance = playersContainers[seat].querySelector(
+  //   "div > div:nth-child(2) > div:nth-child(3) > div.f1ay1w8p.leftPlayer.notZone > div.f11rr7sf.isNotMyPlayer > span"
+  // );
+  var balance = playersContainers[seat].querySelector(
+    "div > div:nth-child(2) > div:nth-child(2) > div.f1ay1w8p.leftPlayer.notZone > div.f11rr7sf.isNotMyPlayer > span"
+  );
+  if (balance) {
+    player.stack = balance.innerText;
   }
-  //  console.log(card);
-  if (card != "card-1") return true;
-  else return false;
+
+  return player;
 }
 
 //function that initializes the seats and players in each seat.
 function initSeats() {
-  //gets position of myplayer
-  const myPlayer = game.getElementsByClassName(
-    "f1phzx2y Desktop landscape myPlayer"
-  )[0];
-  var playerPosition = myPlayer.dataset.qa.slice(-1);
-
-  //gets all seats at the table
-  const players = game.getElementsByClassName("f1phzx2y Desktop landscape");
-
-  //initialize each seat and player
-  for (let i = 0; i < players.length; i++) {
-    var player;
-    var seat;
-
-    if (players[i].getElementsByClassName("feis0ob Desktop")[0]) {
-      var player = new Player(0, 0, false, false);
-      console.log(player);
+  var player;
+  for (var i = 0; i < playersContainers.length; i++) {
+    if (
+      playersContainers[i].querySelector(
+        "div > div:nth-child(2) > div.feis0ob.Desktop"
+      )
+    ) {
+      player = new Player(0, 0, "NOT IN HAND", "INACTIVE");
     } else {
-      var inHand = isInHand(players[i]);
-      console.log("Active Player + " + (i + 1) + " " + inHand);
+      player = initPlayer(i);
     }
+    players[i] = player;
+    // console.log(playersContainers[i]);
   }
+  console.log(players);
+}
+
+//returns when activePlayer is user
+function activePlayer() {
+  isActive = game.querySelector(
+    `#root > div > div.frlfvhr > div.f1l5nl24 > div.fmyv4dc > div.f1qy5s7k > div:nth-child(1) > div.fsusjyu.Desktop.landscape.f1u9jrie > div.f1so0fyt > div:nth-child(${
+      myPlayer + 2
+    }) > div > div:nth-child(2) > div:nth-child(3) > div.f1p7lubp.Desktop.landscape.leftPlayer.notZone.myPlayer > div.f1qbx4ty.Desktop.f1cwv3sl`
+  );
+
+  return true;
 }
 
 //returns whether or not a given player is in the hand
@@ -82,9 +89,9 @@ function main() {
   init();
   initSeats();
 }
-
 // Run the check every 1 seconds (1000 milliseconds)
-setInterval(main, 1000);
+setInterval(activePlayer, 1000);
+setInterval(init, 1000);
 
 class Player {
   constructor(bet, stack, isActive, inHand) {
